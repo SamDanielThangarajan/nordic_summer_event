9b4f54c2e2d9834aedbce14332c6e4ac6dffcb95
Nagarajan Ganesan <nagarajan.ganesan@ericsson.com>
2016-04-28 12:15:43 +0200

    Add simulator method

--- crisis_simulator.py
+++ crisis_simulator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import json
+import time
 import argparse
 
 def parseArgs():
@@ -11,14 +12,20 @@ def parseArgs():
                             help='Seed for the randomizer.')
    parser.add_argument('-f', '--json-file', dest='inventory_file', action='store',
                             help='File Name for the inventory.')
-   parser.add_argument('-i', '--iter-timeout', dest='inventory_file', action='store',
-                            help='File Name for the inventory.')
+   parser.add_argument('-i', '--iter-timeout', dest='iter_timeout', action='store',
+                            help='Timeout for populating db.')
 
    return parser.parse_args()
 
 
+def simulate(iteration, max_users, seed):
+    print "Iteration:%d, Simulating " % iteration
+
 
 if __name__ == "__main__":
    options = parseArgs()
-
-   print options
+   iteration = 0
+   while True:
+       iteration = iteration + 1
+       simulate(iteration, options.number_users, options.seed)
+       time.sleep(float(options.iter_timeout))
