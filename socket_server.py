a3a74357c16bc3c67c6c6b4163304b0ea044285b
SamDanielThangarajan <samdaniel.thangarajan@gmail.com>
2016-04-28 11:34:03 +0200

    some additional

--- socket_server.py
+++ socket_server.py
@@ -1,16 +1,95 @@
 # Socket server in python using select function
  
 import socket, select
+import re
+
+########################################################
+### Class Subscriber
+###
+
+class Subscriber:
+    def __init__(self, number, name, age, loc, skills):
+        self.number = number
+        self.name = name
+        self.age = age
+        self.loc = loc
+        self.skills = skills
+
+    def print_it(self):
+        print ''
+        print 'name> ' + self.name
+        print 'no> ' + self.number
+        print 'age> ' + self.age
+        print 'loc> ' + self.loc
+        print 'skills> ' + self.skills
+        print ''
+
+########################################################
+### Subscriber DB Functions
+###
+
+subs_db = {}
+
+def populate_subscriber_db():
+    global subs_db
+    with open('subscriber_profiles') as file_db:
+        for line in file_db.readlines():
+            """ A_Sam|29|0725838333|ZoneA|engineer_softwaredeveloper_firstaid """
+            match = re.match('([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)\|([^\|]+)',line)
+            if match:
+                subscriber = Subscriber(match.group(3),match.group(1),match.group(2),match.group(4),match.group(5))
+                subs_db[subscriber.number] = subscriber
+
+
+def print_subscriber_db():
+    for k,v in subs_db.items():
+        v.print_it()
+
+
+########################################################
+### Business Logic
+###
 
 def process_data(data):
-    if data.startswith('provide'):
-        with open('inventory.txt', 'a') as the_file:
-            the_file.write(data)
+    if data.startswith('S_'):
+        """ Format:
+        S_Contact_Item_quantity
+        """
+        """ S_number_item_qty """
 
+        match = re.match('S_([^_]+)_([^_]+)_([^_]+)',data)
+
+        if match:
+            sub = subs_db[match.group(1)]
+
+            with open('inventory.txt', 'a') as the_file:
+                input1 = 'Supply_' + sub.number + '_' + sub.loc + '_' + match.group(2) + '_' + match.group(3)
+                the_file.write(input1)
+    elif data.startswith('D_'):
+        """ Format:
+        D_Contact_Item_Quantity
+        """
+        match = re.match('D_([^_]+)_([^_]+)_([^_]+)',data)
+
+        if match:
+            sub = subs_db[match.group(1)]
+            with open('inventory.txt', 'a') as the_file:
+                input1 = 'Demand_' + sub.number + '_' + sub.loc + '_' + match.group(2) + '_' + match.group(3)
+                the_file.write(input1)
+
+
+
+########################################################
+### Server Socket functionality
+###
 
   
 if __name__ == "__main__":
-      
+    
+    populate_subscriber_db()
+    print_subscriber_db()
+
+
     CONNECTION_LIST = []    # list of socket clients
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
     PORT = 5000
